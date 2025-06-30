c14cdc2be9373dcfe4c0373d2b119b46d61e79c6
wen952777 <127197698+wen952777@users.noreply.github.com>
2025-06-30 21:11:59 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GameTable from "./components/GameTable";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>多人斗地主</h1>
+      <GameTable />
+    </div>
+  );
+}
+
+export default App;
