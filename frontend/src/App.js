d29f3341f75f8c6ea901bf34cdf72fb2fdac79c2
wen952777 <127197698+wen952777@users.noreply.github.com>
2025-06-30 21:27:02 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import GameTable from "./components/GameTable";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>多人斗地主</h1>
-      <GameTable />
+      <p>欢迎来到多人斗地主游戏！</p>
     </div>
   );
 }
